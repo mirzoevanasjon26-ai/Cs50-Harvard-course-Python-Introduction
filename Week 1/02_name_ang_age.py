f6411ab95f26598311a47e2257d7788ce9630bc9
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-23 22:52:35 +0300

    Create 02_name_ang_age.py
    WE ARE KEEP GOING 2/4 PER DAY

---
+++ Week 1/02_name_ang_age.py
@@ -0,0 +1,9 @@
+# Function --> it's a action or verb that lets you do something in the program
+# Argument --> it is an input to a function that somehow influence it's behavior
+# Variable --> it's a container for value
+# String --> it's a sequence of text
+
+name = input("What's your name? ")
+age = input("How old are you? ")
+
+print("Hello " + name + ", you are " + age + " years old")
