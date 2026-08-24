c130bbae013a948f0be11764f8f9ded7e0a6435b
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-24 22:24:15 +0300

    Create 07_f_{str}.py
    Completed 3/4

---
+++ Week 1/07_f_{str}.py
@@ -0,0 +1,11 @@
+# Function --> it's a action or verb that lets you do something in the program
+# Argument --> it is an input to a function that somehow influence it's behavior
+# Variable --> it's a container for value
+# String   --> it's a sequence of text
+# sep ""   --> tells print() what to put between multiple values
+# end ""   --> tells print() what to put at the end of the default newline
+# f str    --> tells print(f"hello {name}" ) we use for it {}
+
+
+name = input("What's your name? ")
+print(f"Hello {name}")
