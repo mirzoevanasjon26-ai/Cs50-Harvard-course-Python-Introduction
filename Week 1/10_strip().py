611b7eaf461303998835ffdc2703004238fd0062
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-25 22:20:29 +0300

    Create 10_strip().py
    Todays lesson is str-methods 1/4

---
+++ Week 1/10_strip().py
@@ -0,0 +1,8 @@
+# Strip()         --> remove white space from left to right
+# Capitalize()    --> makes the first charakter uppercase
+# Tittle()        --> makes the fist character of each word uppercase
+
+
+name = input("What's your name? ").strip()
+print(name)
+
