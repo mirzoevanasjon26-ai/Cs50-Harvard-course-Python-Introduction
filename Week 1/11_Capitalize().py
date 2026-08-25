8906ddcec2e59769bc4e9225461421668b9c4f1a
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-25 22:33:12 +0300

    Update 11_Capitalize().py
    Capitalize --> to make it uppercase

--- Week 1/11_Capitalize().py
+++ Week 1/11_Capitalize().py
@@ -1,6 +1,6 @@
 # Strip()         --> remove white space from left to right
 # Capitalize()    --> makes the first charakter uppercase
-# Tittle()        --> makes the fist character of each word uppercase
+# Title()        --> makes the fist character of each word uppercase
 
 name = input("What'a your name? ").capitalize()
 print(name)
