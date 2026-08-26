506799349c54bdaf61d04a7af92870347c4514e4
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-26 20:38:17 +0300

    Create 19_calculator.py
    using int to create calculator

---
+++ Week 1/19_calculator.py
@@ -0,0 +1,10 @@
+# 1-st version calculator
+
+a = int(input("What's a? ")) 
+b = int(input("What's b? ")) 
+print(a+b)
+
+
+# 2-nd version calculator
+
+print(int(input("What's x? ")) + int(input("What's y? ")) )
