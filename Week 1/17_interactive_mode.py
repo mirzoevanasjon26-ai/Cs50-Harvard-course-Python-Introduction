c35c9bc4220918b9770e6a2f82467bbbcc4c6713
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-26 20:10:40 +0300

    Create 17_interactive_mode.py
    is the way run Python commands one at a time and see the result immediately.

---
+++ Week 1/17_interactive_mode.py
@@ -0,0 +1,9 @@
+# interctive mode in Python  
+# is the way run Python commands one at a time and see the result immediately.
+# you mus press ^ and write python start use it 
+
+
+>>> 2+2
+4
+>>> print("hello, word")
+Hello, word
