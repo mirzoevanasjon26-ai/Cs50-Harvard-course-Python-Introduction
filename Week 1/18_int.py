423d03cfddc9e46ce506ec5bdfcd7611161b4578
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-26 20:31:29 +0300

    Create 18_int.py

---
+++ Week 1/18_int.py
@@ -0,0 +1,14 @@
+#  int is Data Type and it can also be as a function
+# int --> integer it is number -2 -1 0 1 2  no decimal point in intiger
+#  +  --> plus
+#  -  --> subtract numbers
+#  *  --> multiply numbers
+#  /  --> divide numbers.
+#  %  --> modulo operator --> the operator that allows you to take the remainder after dividing one number by another.
+
+
+
+
+x = int(input("Whar's the x? "))
+y = int(input("Whar's the y? "))
+print(x + y)
