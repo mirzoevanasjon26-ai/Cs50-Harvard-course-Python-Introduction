acdd901f28fbb4876cbb9b74fd438f35927e1c96
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-27 23:17:54 +0300

    Create 21_round.py
    to round a number to the nearest whole number

---
+++ Week 1/21_round.py
@@ -0,0 +1,8 @@
+# rount --> to round a number to the nearest whole number
+# 5.7 --> 6   /  5.2 ---> 5
+
+
+a = float(input("What's the a? "))
+b = float(input("What's the b? "))
+
+print(round(a+b))
