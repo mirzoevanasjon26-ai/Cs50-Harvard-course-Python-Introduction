1f299b10da3181668144cd9ac871b050b95a26e9
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-27 23:12:33 +0300

    Create 20_float.py
    # float --> it's a data type used to represent numbers with decimal points.

---
+++ Week 1/20_float.py
@@ -0,0 +1,6 @@
+# float --> it's a data type used to represent numbers with decimal points.
+# like this numers 11.2, 3.22, 44.11 
+x = float(input("What'a the x? "))
+y = float(input("What's the y? "))
+
+print(x+y)
