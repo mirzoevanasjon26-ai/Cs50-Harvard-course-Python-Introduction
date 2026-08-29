34d14d870621c0066507e386bc2360959ea3310c
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-29 20:00:17 +0300

    Create 28_def.py
    def we use to create our own function

---
+++ Week 1/28_def.py
@@ -0,0 +1,11 @@
+# def ---> define or create your own function
+#def hello()
+#The parentheses with nothing inside means that this function at the moment is not going to take any inputs, no arguments there too.
+
+def hello():
+    print("hello")
+
+
+name = input("What's  your name? ")
+hello()
+print(name)
