5b5e657fc99dbc03f59df67360206eca275515f3
MIRZOEV ANASJON <mirzoevanasjon26@gmail.com>
2026-08-29 20:15:28 +0300

    Create 29_def_with_seca_argum.py
    we use to ad "to" to make it in one line

---
+++ Week 1/29_def_with_seca_argum.py
@@ -0,0 +1,10 @@
+# def ---> define or create your own function
+#def hello()
+#The parentheses with nothing inside means that this function at the moment is not going to take any inputs, no arguments there too.
+# to is parameter --> a variable indife the parentheses of function that recieve a value when the funnction is called
+def hello(to):
+    print("hello", to)
+
+
+name = input("What's  your name? ")
+hello(name)
